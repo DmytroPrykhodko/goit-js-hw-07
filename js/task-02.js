99ebc78fed7e4231f8057e4429d35baef2911e16
Prykhodko Dmytro <prykhodko.dmytro1995@gmail.com>
2021-08-10 14:31:09 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,3 +8,15 @@ const ingredients = [
 ];
 
 const ingredientsEl = document.querySelector('#ingredients');
+
+const makeElementsItem = options => {
+  return options.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('ingredients__item');
+  itemEl.textContent = ingredient;
+  return itemEl;
+  });
+
+    };
+const elementsItem = makeElementsItem(ingredients);
+ingredientsEl.append(...elementsItem);
